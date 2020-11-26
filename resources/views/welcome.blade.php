bc617dedd7b5d31768ca5e7aedf694a79f5e2a6c
Makeart03 <mtz.ruiz.dorina.28@gmail.com>
2020-11-26 00:08:26 -0600

    Initial Commit

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,58 +1,283 @@
 <!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-<head>
-
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-
-    <title>PC Container</title>
-
-    <!-- Fonts -->
-    <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
-
-
-    <!-- BootsTrap -->
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css"
-          integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
-    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js"
-            integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN"
-            crossorigin="anonymous"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js"
-            integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q"
-            crossorigin="anonymous"></script>
-    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"
-            integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl"
-            crossorigin="anonymous"></script>
-    <!-- Fin de BootsTrap -->
-
-</head>
-<body>
-<div class="flex-center position-ref full-height">
-    @if (Route::has('login'))
-        <div class="top-right links">
-            @auth
-                <a href="{{ url('/home') }}">Home</a>
-            @else
-                <a href="{{ route('login') }}">Iniciar Sesión</a>
-
-                @if (Route::has('register'))
-                    <a href="{{ route('register') }}">Registrar Usuario</a>
-                @endif
-            @endauth
-        </div>
-    @endif
-
-
-    <div class="content">
-        <div class="title m-b-md" align="center">
-            <h1>PC Container </h1>
-            <h2>Hello, world!</h2>
-        </div>
-
-
-    </div>
-</div>
-</body>
-
-
-
+<html lang="en">
+   <head>
+      <!-- basic -->
+      <meta charset="utf-8">
+      <meta http-equiv="X-UA-Compatible" content="IE=edge">
+      <!-- mobile metas -->
+      <meta name="viewport" content="width=device-width, initial-scale=1">
+      <meta name="viewport" content="initial-scale=1, maximum-scale=1">
+      <!-- site metas -->
+      <title>PC Container</title>
+      <meta name="keywords" content="">
+      <meta name="description" content="">
+      <meta name="author" content="">
+      <!-- bootstrap css -->
+      <link rel="stylesheet" href="css/bootstrap.min.css" />
+      <!-- site css -->
+      <link rel="stylesheet" href="css/style.css" />
+      <!-- responsive css -->
+      <link rel="stylesheet" href="css/responsive.css" />
+      <!-- colors css -->
+      <link rel="stylesheet" href="css/colors.css" />
+      <!-- wow animation css -->
+      <link rel="stylesheet" href="css/animate.css" />
+      <!--[if lt IE 9]>
+      <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+      <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
+      <![endif]-->
+   </head>
+   <body id="default_theme" class="home_page1">
+      <!-- header -->
+      <header class="header header_style1">
+         <div class="container">
+            <div class="row">
+               <div class="col-md-9 col-lg-10">
+                  <div class="logo"><a href="index.html"><img src="img/2.png" alt="#" /></a></div>
+               </div>
+               <div class="col-md-3 col-lg-2">
+                  <div class="right_bt"><a class="bt_main" href="{{ route('home') }}">Ingresa</a> </div>
+               </div>
+            </div>
+         </div>
+      </header>
+      <section id="banner_parallax" class="slide_banner1">
+         <div class="container">
+            <div class="row">
+               <div class="col-md-6">
+                  <div class="full">
+                     <div class="slide_cont">
+                        <h2>LLeva un mejor control de tu inventario y de tu negocio</h2>
+                        <p>Utilizando una aplicación diseñada para ti</p>
+                     </div>
+                  </div>
+               </div>
+               <div class="col-md-6">
+                  <div class="full">
+                     <div class="slide_pc_img wow fadeInRight" data-wow-delay="1s" data-wow-duration="2s"> <img src="img/pc-banner.png" alt="#" /> </div>
+                  </div>
+               </div>
+            </div>
+         </div>
+      </section>
+      <!-- end header -->
+      <!-- section -->
+      <section class="layout_padding padding_top_0">
+         <div class="container">
+            <div class="row margin_bottom_30">
+               <!-- featured cont -->
+               <div class="col-md-6 cont_theme_blog">
+                  <div class="full">
+                     <h3>"Tenemos que dejar de optimizar para programadores y comenzar a optimizar para usuarios"</h3>
+                     <p>Jeff Atwood</p>
+                  </div>
+               </div>
+               <!-- end featured cont -->
+               <!-- featured image -->
+               <div class="col-md-6 wow fadeInRight" data-wow-delay="0.5" data-wow-duration="1s">
+                  <div class="full">
+                     <div class="center"><img src="img/ser2.png" alt="#" /></div>
+                  </div>
+               </div>
+               <!-- end featured image -->
+            </div>
+         </div>
+      </section>
+      <!-- end section -->
+      <!-- section -->
+      <section class="layout_padding gradiant_bg cross_layout">
+         <div class="container">
+            <div class="row">
+               <div class="col-md-4">
+                  <div class="information_blogs">
+                     <div class="imf_icon"><img src="img/icon1.png" alt="#" /></div>
+                     <div class="imf_head">
+                        <h3>Ajustable a tus necesidades</h3>
+                     </div>
+                     <div class="imf_cont">
+                        <p>Esta adecuado para cumplir las necesidades de llevar un control de inventario en cualquier taller de servicios de computo</p>
+                     </div>
+                  </div>
+               </div>
+               <div class="col-md-4">
+                  <div class="information_blogs">
+                     <div class="imf_icon"><img src="img/icon2.png" alt="#" /></div>
+                     <div class="imf_head">
+                        <h3>Diseño creativo</h3>
+                     </div>
+                     <div class="imf_cont">
+                        <p>Cuenta con un diseño de fácil entendimiento para el usuario</p>
+                     </div>
+                  </div>
+               </div>
+               <div class="col-md-4">
+                  <div class="information_blogs">
+                     <div class="imf_icon"><img src="img/icon3.png" alt="#" /></div>
+                     <div class="imf_head">
+                        <h3>Acceso las 24 hrs del día</h3>
+                     </div>
+                     <div class="imf_cont">
+                        <p>Siempre que cuente con su cuenta de usuario puede acceder a la información dentro del sistema</p>
+                     </div>
+                  </div>
+               </div>
+            </div>
+         </div>
+      </section>
+      <!-- end section -->
+      <!-- section -->
+      <section class="layout_padding layer_style">
+         <div class="container">
+            <div class="row">
+               <div class="col-sm-12">
+                  <div class="full text_align_center">
+                     <div class="heading_main center_head_border heading_style_1">
+                        <h2>Características</h2>
+                     </div>
+                  </div>
+               </div>
+            </div>
+            <div class="row app-features">
+               <div class="col-md-4 col-sm-6 col-xs-12">
+                  <ul class="features-left">
+                     <li>
+                        <i class="flaticon-coding"></i>
+                        <div class="fl-inner">
+                           <h4>Gestión de equipos en taller</h4>
+                           <p>Tipo de equipo, marca, modelo.</p>
+                        </div>
+                     </li>
+                     <li>
+                        <i class="flaticon-customer-service"></i>
+                        <div class="fl-inner">
+                           <h4>Órdenes de reparación</h4>
+                           <p>Información del cliente, datos del equipo incluyendo características. </p>
+                        </div>
+                     </li>
+                     <li>
+                        <i class="flaticon-devices"></i>
+                        <div class="fl-inner">
+                           <h4>Seguimiento de equipos en taller</h4>
+                           <p>Contando con filtro por estado, técnico, cliente y fecha. </p>
+                        </div>
+                     </li>
+                     <li>
+                        <i class="flaticon-support"></i>
+                        <div class="fl-inner">
+                           <h4>Servicios a Domicilio</h4>
+                           <p>Órdenes de servicio, información del cliente, problemas que reporta, datos del equipo.</p>
+                        </div>
+                     </li>
+                  </ul>
+               </div>
+               <div class="col-md-4 col-sm-6 col-xs-12">
+                  <ul class="features-right">
+                     <li>
+                        <i class="flaticon-house"></i>
+                        <div class="fr-inner">
+                           <h4>Seguimiento de servicios a domicilio</h4>
+                           <p>Contando con filtro por estado, técnico asignado, cliente y fecha. </p>
+                        </div>
+                     </li>
+                     <li>
+                        <i class="flaticon-new-file"></i>
+                        <div class="fr-inner">
+                           <h4>Informes</h4>
+                           <p>Acceso a reportes de diferentes campos: Clientes, Servicios, Componentes vendidos</p>
+                        </div>
+                     </li>
+                     <li>
+                        <i class="flaticon-line-graph"></i>
+                        <div class="fr-inner">
+                           <h4>Productividad y Gestión</h4>
+                           <br>Acceso solo a técnicos registrados.</br>
+                           <br>Número de servicios por técnico.</br>
+                           <br>Número de servicios por estado.</br>
+                           <br>Gráficas Anuales, por estado de servicio.</br>
+                        </div>
+                     </li>
+                  </ul>
+               </div>
+               <div class="col-md-4 wow fadeInRight" data-wow-delay="0.5" data-wow-duration="1s">
+                  <div class="full">
+                     <div class="center">
+                        <img src="img/application_screen.png" alt="#" />
+                     </div>
+                  </div>
+               </div>
+            </div>
+         </div>
+      </section>
+      <!-- end section -->
+      <!-- section -->
+      <section class="contact_section">
+         <div class="container-fluid">
+            <div class="row">
+                <div class="col-xs-12 col-sm-12 col-md-6" style="padding:0;">
+                    <img src="img/scr4.png" />
+                </div>
+               <div class="col-xs-12 col-sm-12 col-md-6" style="padding:0;">
+                  <div class="full">
+                     <div class="contact_form white_heading_border">
+                        <div class="contact_form_inner">
+                           <div class="full_heading white_fonts heading_main heading_style_1">
+                              <h2>Contáctanos</h2>
+                           </div>
+                           <p>Compartenos tus experiencias con PC Container</p>
+                           <div class="form_contact">
+                              <!-- form -->
+                              <form action="index.html">
+                                 <fieldset>
+                                    <div class="field">
+                                       <input type="text" name="name" placeholder="Your name" required="">
+                                    </div>
+                                    <div class="field">
+                                       <input type="text" name="name" placeholder="Your phone" required="">
+                                    </div>
+                                    <div class="field">
+                                       <input type="email" name="email" placeholder="Email" required="">
+                                    </div>
+                                    <div class="field">
+                                       <textarea name="messager" placeholder="Messager" required=""></textarea>
+                                    </div>
+                                    <div class="field">
+                                       <button class="field_bt">Send</button>
+                                    </div>
+                                 </fieldset>
+                              </form>
+                              <!-- end form -->
+                           </div>
+                        </div>
+                     </div>
+                  </div>
+               </div>
+            </div>
+         </div>
+      </section>
+      <!-- end section -->
+      <!-- footer -->
+      <footer class="footer_style_2">
+         <div class="footer_top">
+            <div class="container">
+            <div class="footer-copyright text-center py-3">© 2020 Copyright:
+                    <a href="http://www.itsteziutlan.edu.mx/"> Instituto Tecnológico Superior de Teziutlán</a>
+                    <br> Control y registro de inventario en Empresas de Servicio de Cómputo - PC Container
+                </div>
+            </div>
+         </div>
+      </footer>
+      <!-- end footer -->
+      <!--=========== js section ===========-->
+      <!-- jQuery (necessary for Bootstrap's JavaScript) -->
+      <script src="js/jquery.min.js"></script>
+      <script src="js/popper.min.js"></script>
+      <script src="js/bootstrap.min.js"></script>
+      <!-- wow animation -->
+      <script src="js/wow.js"></script>
+      <!-- custom js -->
+      <script src="js/custom.js"></script>
+      <!-- google map js -->
+      <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyA8eaHt9Dh5H57Zh0xVTqxVdBFCvFMqFjQ&callback=initMap"></script>
+      <!-- end google map js -->
+   </body>
+</html>
